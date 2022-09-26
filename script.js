872943ae80a0d075cca54e2c022a3cf8c74b5d74
irakli-dadu <lipsowow@gmail.com>
2022-09-26 23:09:48 +0400

    not finished
    ...

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
 let messageEl = document.getElementById("message-El");
@@ -11,11 +10,49 @@ let sumEl = document.getElementById("sum-El");
 
 let cardsEl = document.getElementById("cards-el");
 
+let playerEl = document.getElementById("player-el");
+
+let player = {
+    name: "Per",
+    chips: 200
+}
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 
-function startGame() {
+function startGame () {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+
+
+    renderGame()
+}
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random()*13) + 1;
+
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber
+    }
+}
+
+
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum;
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    cardsEl.textContent = "Cards: ";
+
+    for (i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     if(sum <= 20) {
         message = "Do you want to draw a new Card?"
     }else if (sum === 21) {
@@ -29,5 +66,12 @@ function startGame() {
 }
 
 function newCard() {
-    console.log("new card on the deck");
+    if (isAlive === true && hasBlackjack === false) {
+
+        let card = getRandomCard();
+        sum += card;
+    
+        cards.push(card);
+        renderGame()
+    }
 }
